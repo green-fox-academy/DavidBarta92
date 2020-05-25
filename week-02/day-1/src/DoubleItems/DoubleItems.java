7185b442894dbb04bdabc9de88d26aebebea09fa
DavidBarta92 <blj.david@gmail.com>
2020-05-25 11:33:41 +0100

    something is wrong here

---
+++ week-02/day-1/src/DoubleItems/DoubleItems.java
@@ -0,0 +1,12 @@
+package DoubleItems;
+
+public class DoubleItems {
+    public static void main(String[] args) {
+        int[] numList = {3, 4, 5, 6, 7};
+
+        for (byte i  = 0; i < numList.length; i++){
+            numList[i] = numList[i] * 2;
+            System.out.println(numList[i]);
+        }
+    }
+}
