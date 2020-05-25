4b1c8cdd792983308258ad8d15049ace937321e3
DavidBarta92 <blj.david@gmail.com>
2020-05-25 15:33:31 +0100

    Reverse.java is finally fixed

--- week-02/day-1/src/Reverse/Reverse.java
+++ week-02/day-1/src/Reverse/Reverse.java
@@ -3,14 +3,13 @@ package Reverse;
 public class Reverse {
     public static void main(String[] args) {
         byte[] numbers = {3, 4, 5, 6, 7};
+        byte[] reverseNumbers = {0, 0, 0, 0, 0};
         for (byte i = 0; i < numbers.length; i++) {
-            byte temp = numbers[i];
-            numbers[i]= numbers[((byte) numbers.length)-i-1];
-            numbers[((byte) numbers.length)-i-1]= temp;
-            System.out.println(numbers[i]);
+            reverseNumbers[i] = numbers[numbers.length-1-i];
         }
         for (byte i = 0; i < numbers.length; i++) {
-// nem jo
+            numbers[i] = reverseNumbers[i];
+            System.out.println(numbers[i]);
         }
     }
 }
