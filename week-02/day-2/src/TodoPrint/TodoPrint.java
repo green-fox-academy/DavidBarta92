bb645d181f452a61c5b5232fcb093337c9b8ce75
DavidBarta92 <blj.david@gmail.com>
2020-05-26 15:10:22 +0100

    added TodoPrint

---
+++ week-02/day-2/src/TodoPrint/TodoPrint.java
@@ -0,0 +1,9 @@
+package TodoPrint;
+
+public class TodoPrint {
+    public static void main(String[] args) {
+        String todoText = " - Buy milk\n";
+        todoText = "My todo:\n"+todoText+" - Download games\n\t - Diablo";
+        System.out.println(todoText);
+    }
+}
