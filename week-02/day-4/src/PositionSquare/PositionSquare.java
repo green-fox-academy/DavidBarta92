a2f7eae8e1d15aab883b250e569c7833c4035655
DavidBarta92 <blj.david@gmail.com>
2020-05-28 14:14:22 +0100

    PositionSquare is done

---
+++ week-02/day-4/src/PositionSquare/PositionSquare.java
@@ -0,0 +1,46 @@
+package PositionSquare;
+
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class PositionSquare {
+
+    public static void mainDraw(Graphics graphics){
+        for (byte i = 0; i < 3; i++){
+            int x = 50;
+            int y = 50;
+            squareDrawer(x, y, graphics);
+        }
+    }
+    public static void squareDrawer(int x, int y, Graphics graphics){
+        graphics.setColor(new Color(99,100,202,150));
+        graphics.fillRect(x, y, 50, 50);
+    }
+
+    // Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 320;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        ImagePanel panel = new ImagePanel();
+        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.add(panel);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+        jFrame.pack();
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
+
+}
