6474fef835c7ecdbf44438e8da9cfe5845485cdb
DavidBarta92 <blj.david@gmail.com>
2020-05-30 20:41:05 +0100

    finished ConnectTheDots

--- week-02/day-5/src/ConnectTheDots/ConnectTheDots.java
+++ week-02/day-5/src/ConnectTheDots/ConnectTheDots.java
@@ -3,24 +3,31 @@ package ConnectTheDots;
 import javax.swing.*;
 
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
 public class ConnectTheDots {
     public static void mainDraw(Graphics graphics) {
-        // Create a function that takes 2 parameters:
-        // An array of {x, y} points and graphics
-        // and connects them with green lines.
-        // Connect these to get a box: {{10, 10}, {290,  10}, {290, 290}, {10, 290}}
-        // Connect these: {{50, 100}, {70, 70}, {80, 90}, {90, 90}, {100, 70},
-        // {120, 100}, {85, 130}, {50, 100}}
-        int[] points = {1, 2};
-
+        List<List<Integer>> boxPoints = new ArrayList<>(Arrays.asList(Arrays.asList(10, 10), Arrays.asList(290,  10),
+                Arrays.asList(290, 290), Arrays.asList(10, 290)));
+        drawLine(boxPoints, graphics);
+        List<List<Integer>> otherPoints = new ArrayList<>(Arrays.asList(Arrays.asList(50, 100), Arrays.asList(70, 70),
+                Arrays.asList(80, 90), Arrays.asList(90, 90), Arrays.asList(100, 70), Arrays.asList(120, 100),
+                Arrays.asList(85, 130), Arrays.asList(50, 100)));
+        drawLine(otherPoints, graphics);
     }
-    public static void connect(int[] points, Graphics graphics){
-
+    public static void drawLine(List<List<Integer>> coordinates, Graphics graphics){
+        for (int i = 0; i < coordinates.size() - 1; i++) {
+            graphics.setColor(Color.GREEN);
+            graphics.drawLine(coordinates.get(i).get(0), coordinates.get(i).get(1), coordinates.get(i + 1).get(0),
+                    coordinates.get(i + 1).get(1));
+        }
+        graphics.drawLine(coordinates.get(coordinates.size() - 1).get(0), coordinates.get(coordinates.size() - 1).get(1),
+                coordinates.get(0).get(0), coordinates.get(0).get(1));
     }
-
     // Don't touch the code below
     static int WIDTH = 320;
     static int HEIGHT = 320;
