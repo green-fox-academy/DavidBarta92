282bd9d724c83f5d871ba577b1d4a0868619f7e5
DavidBarta92 <blj.david@gmail.com>
2020-06-03 13:36:24 +0100

    postIt is finished

--- week-03/day-3/src/postIt/postIt.java
+++ week-03/day-3/src/postIt/postIt.java
@@ -1,15 +1,21 @@
 package postIt;
 
-public class postIt {
-    String backgroundColor;
-    String text;
-    String textColor;
+import java.awt.*;
 
-    public postIt(String backgroundColor, String text, String textColor){
+public class postIt {
+    Color backgroundColor;
+    String text;
+    Color textColor;
+
+    public postIt(Color backgroundColor, String text, Color textColor){
         this.backgroundColor= backgroundColor;
         this.text = text;
         this.textColor = textColor;
     }
 
-    
+    public static void main(String[] args) {
+        postIt note1 = new postIt(Color.orange,"Idea 1",Color.blue);
+        postIt note2= new postIt(Color.pink,"Awesome",Color.black);
+        postIt note3= new postIt(Color.yellow,"Superb!",Color.green);
+    }
 }
