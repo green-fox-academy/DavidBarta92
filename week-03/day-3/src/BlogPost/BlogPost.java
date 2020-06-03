c8ecfb8472a7e758529d15af6f1adb7e2653769a
DavidBarta92 <blj.david@gmail.com>
2020-06-03 13:51:43 +0100

    added BlogPost package

---
+++ week-03/day-3/src/BlogPost/BlogPost.java
@@ -0,0 +1,15 @@
+package BlogPost;
+
+public class BlogPost {
+    String authorName;
+    String title;
+    String text;
+    String date;
+
+    public BlogPost (String authorName, String title, String text, String date){
+        this.authorName = authorName;
+        this.title = title;
+        this.text = text;
+        this.date = date;
+    }
+}
