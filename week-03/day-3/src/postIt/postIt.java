b717b4fd83ae38bc59f4303c8c44255241a29791
DavidBarta92 <blj.david@gmail.com>
2020-06-03 13:15:57 +0100

    added d3 folder

---
+++ week-03/day-3/src/postIt/postIt.java
@@ -0,0 +1,15 @@
+package postIt;
+
+public class postIt {
+    String backgroundColor;
+    String text;
+    String textColor;
+
+    public postIt(String backgroundColor, String text, String textColor){
+        this.backgroundColor= backgroundColor;
+        this.text = text;
+        this.textColor = textColor;
+    }
+
+    
+}
