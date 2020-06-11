224146d50fac8980c993f89ccfafcba6a9636d4e
DavidBarta92 <blj.david@gmail.com>
2020-06-11 13:55:39 +0100

    SolarSystem done

--- week-04/day-4/src/Reverse/Reverse.java
+++ week-04/day-4/src/Reverse/Reverse.java
@@ -1,12 +1,11 @@
+package Reverse;
+
 import java.io.IOException;
-import java.lang.reflect.Array;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import java.util.Scanner;
 
 public class Reverse {
 
