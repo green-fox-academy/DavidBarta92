9508f6e438a1867be6d6a615ea9442b3628d30f9
DavidBarta92 <blj.david@gmail.com>
2020-06-29 15:31:47 +0100

    ex5 done

--- week-07/day-1/src/labdaNstream/exercise4.java
+++ week-07/day-1/src/labdaNstream/exercise4.java
@@ -4,11 +4,11 @@ import java.util.Arrays;
 import java.util.List;
 
 public class exercise4 {
-  List<Integer> numbers = Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14);
+  static List<Integer> numbers = Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14);
 
   public static void main(String[] args) {
     numbers.stream()
-        .filter(n -> n%2 !=0)
-        .forEach(n -> (n + n+1));
+        .filter(n -> n%2 !=0);
+        //.forEach(n -> (n + n+1));
   }
 }
