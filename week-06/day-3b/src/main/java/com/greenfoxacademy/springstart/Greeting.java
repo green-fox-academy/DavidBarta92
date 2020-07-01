aa2e9e4f842c82f9d74e38c373063b6503e7d949
DavidBarta92 <blj.david@gmail.com>
2020-07-01 15:30:13 +0100

    helloworldRestApp done

---
+++ week-06/day-3b/src/main/java/com/greenfoxacademy/springstart/Greeting.java
@@ -0,0 +1,17 @@
+package com.greenfoxacademy.springstart;
+
+public class Greeting {
+  long id = 1;
+  String content = "Hello, World!";
+
+  public void Greeting(){
+  }
+
+  public long getId(){
+    return this.id;
+  }
+
+  public String getContent(){
+    return this.content;
+  }
+}
