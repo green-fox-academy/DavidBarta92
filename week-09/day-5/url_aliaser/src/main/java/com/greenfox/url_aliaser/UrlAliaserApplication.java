06c981e4044db26d218b2de270dd6820591c0007
DavidBarta92 <blj.david@gmail.com>
2020-07-24 13:27:06 +0200

    entryService injected

--- week-09/day-5/url_aliaser/src/main/java/com/greenfox/url_aliaser/UrlAliaserApplication.java
+++ week-09/day-5/url_aliaser/src/main/java/com/greenfox/url_aliaser/UrlAliaserApplication.java
@@ -1,13 +1,20 @@
 package com.greenfox.url_aliaser;
 
+import com.greenfox.url_aliaser.Service.EntryService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class UrlAliaserApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(UrlAliaserApplication.class, args);
+  }
+
+  @Bean
+  EntryService articleService(){
+    return new EntryService();
   }
 
 }
