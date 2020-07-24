f33f33512bd9d7b18d3c8be9e8b337f8bfe8c503
DavidBarta92 <blj.david@gmail.com>
2020-07-24 13:24:41 +0200

    repo done

--- week-09/day-5/url_aliaser/src/main/java/com/greenfox/url_aliaser/Repository/EntryRepository.java
+++ week-09/day-5/url_aliaser/src/main/java/com/greenfox/url_aliaser/Repository/EntryRepository.java
@@ -1,4 +1,9 @@
 package com.greenfox.url_aliaser.Repository;
 
-public class EntryRepository {
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import sun.rmi.runtime.Log;
+
+@Repository
+public interface EntryRepository extends CrudRepository<Log, Long> {
 }
