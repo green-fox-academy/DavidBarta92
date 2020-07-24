540121eea2c56f9cbf0a30e47d068448fda3a697
DavidBarta92 <blj.david@gmail.com>
2020-07-24 13:26:20 +0200

    entryService done

--- week-09/day-5/url_aliaser/src/main/java/com/greenfox/url_aliaser/Service/EntryService.java
+++ week-09/day-5/url_aliaser/src/main/java/com/greenfox/url_aliaser/Service/EntryService.java
@@ -1,5 +1,22 @@
 package com.greenfox.url_aliaser.Service;
 
+import com.greenfox.url_aliaser.Repository.EntryRepository;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@Getter
+@Setter
+@NoArgsConstructor
 public class EntryService {
+  @Autowired
+  private EntryRepository entryRepository;
+
+  public EntryService(EntryRepository entryRepository) {
+    this.entryRepository = entryRepository;
+  }
 
 }
