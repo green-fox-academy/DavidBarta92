aec1f3a4e52b9e53179e773205e1f4f54a502a5a
DavidBarta92 <blj.david@gmail.com>
2020-07-24 13:17:47 +0200

    all of the endpoints added

--- week-09/day-5/url_aliaser/src/main/java/com/greenfox/url_aliaser/Controllers/MainController.java
+++ week-09/day-5/url_aliaser/src/main/java/com/greenfox/url_aliaser/Controllers/MainController.java
@@ -1,13 +1,26 @@
 package com.greenfox.url_aliaser.Controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class MainController {
 
   @GetMapping("/")
-  public String index(){
+  public String main() {
     return "main";
+  }
+
+  @PostMapping("/save-link")
+  public String save(@RequestParam(name = "url") String url,
+                     @RequestParam(name = "alias") String alias) {
+
+    return "redirect:/";
+  }
+
+  @GetMapping("/a/{alias}")
+  public String a(@PathVariable(name = "alias") String alias) {
+
+    return "redirect:/";
   }
 }
--- week-09/day-5/url_aliaser/src/main/java/com/greenfox/url_aliaser/Controllers/MainRestController.java
+++ week-09/day-5/url_aliaser/src/main/java/com/greenfox/url_aliaser/Controllers/MainRestController.java
@@ -1,7 +1,26 @@
 package com.greenfox.url_aliaser.Controllers;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
 
 @RestController
 public class MainRestController {
+
+  @GetMapping(value = "/api/links")
+  public ResponseEntity<?> links() {
+    try {
+      //return ResponseEntity.status(200).body(new LogReport(logService.getAll(), logService.getAll().size()));
+    } catch (Exception e400) {
+      //return ResponseEntity.status(500).body(new ErrorMessage("Please provide an input!"));
+    }
+    return null;
+  }
+
+  @DeleteMapping("/api/links/{id}")
+  public String delete(@PathVariable(name = "id") Long id) {
+    
+    return null;
+  }
 }
