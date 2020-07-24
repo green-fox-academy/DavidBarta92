ca992992ab2d81b2ed1464f4a7c299ee4d34106a
DavidBarta92 <blj.david@gmail.com>
2020-07-24 13:23:25 +0200

    entry class fields

--- week-09/day-5/url_aliaser/src/main/java/com/greenfox/url_aliaser/Model/Entry.java
+++ week-09/day-5/url_aliaser/src/main/java/com/greenfox/url_aliaser/Model/Entry.java
@@ -1,4 +1,29 @@
 package com.greenfox.url_aliaser.Model;
 
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
 public class Entry {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  private String url;
+  private String alias;
+  private int secretCode;
+  private int hitCount;
+
+
 }
